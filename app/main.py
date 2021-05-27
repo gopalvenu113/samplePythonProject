44ec3ee86bb36e28fee540d1e89d55f9d6a714a2
venugopal <venugopal.reddy@onsite.co.in>
2021-05-27 09:36:18 +0530

    Added data file and requirements

---
+++ app/main.py
@@ -0,0 +1,13 @@
+
+
+'''
+    Requirements to perform calculation on the given data.
+    1. Find employee count whose age is greated than 30.
+    2. Calculate appraisals for employees:
+        a. for employees with sal more than 1lakh - 10% hike.
+        b. for employees with less than 1lakh - 20% hike.
+    3. Find employees distributed amoung regions.
+'''
+
+if __name__=='__main__':
+    pass
